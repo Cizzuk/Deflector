ab99fe844bca0f51bbfdb8f19f0450ed900e607f
Cizzuk <dropcat@cizzuk.net>
2026-08-11 19:30:13 +0900

    Update UserNotificationSupport.swift

--- Deflector/Deflector/Models/Supports/UserNotificationSupport.swift
+++ Deflector/Deflector/Models/Supports/UserNotificationSupport.swift
@@ -61,8 +61,20 @@ class UserNotificationSupport {
     
     static func sendDeflectionNotification(name: String) async {
         let content = UNMutableNotificationContent()
-        content.title = String(localized: "Running Shortcut")
+        content.title = String(localized: "Deflection")
         content.body = name
+        content.sound = .none
+        content.interruptionLevel = .timeSensitive
+        
+        await sendNotification(content: content)
+    }
+    
+    static func sendDeflectionErrorNotification(reason: LocalizedStringResource? = nil) async {
+        let content = UNMutableNotificationContent()
+        content.title = String(localized: "Deflection failed")
+        if let reason {
+            content.body = String(localized: reason)
+        }
         content.sound = .none
         content.interruptionLevel = .timeSensitive
         
