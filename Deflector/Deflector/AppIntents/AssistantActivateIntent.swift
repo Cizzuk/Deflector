9548274dea3c018d353587dfe30829cb26d9c77a
Cizzuk <dropcat@cizzuk.net>
2026-08-11 18:48:10 +0900

    Create AssistantActivateIntent

---
+++ Deflector/Deflector/AppIntents/AssistantActivateIntent.swift
@@ -0,0 +1,25 @@
+//
+//  AssistantActivateIntent.swift
+//  Deflector
+//
+//  Created by Cizzuk on 2026/08/11.
+//
+
+import AppIntents
+import UserNotifications
+
+@AppIntent(schema: .assistant.activate)
+struct AssistantActivateIntent: AppIntent {
+    static let title: LocalizedStringResource = "Side Button Deflector"
+    static let isDiscoverable = false
+    static var supportedModes: IntentModes = .foreground
+    
+    init() {
+        Self.supportedModes = .background
+    }
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        let content = UNMutableNotificationContent()
+    }
+}
