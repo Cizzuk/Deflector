9c78df25fd76c264b29d60df16f1d433da74a072
Cizzuk <dropcat@cizzuk.net>
2026-08-11 18:38:10 +0900

    Create MainView

---
+++ Deflector/Deflector/Views/MianView.swift
@@ -0,0 +1,21 @@
+//
+//  MianView.swift
+//  Deflector
+//
+//  Created by Cizzuk on 2026/08/11.
+//
+
+import SwiftUI
+
+@main struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MianView()
+        }
+    }
+}
+
+struct MianView: View {
+    var body: some View {
+    }
+}
