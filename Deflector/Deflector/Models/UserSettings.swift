ae990c48c0f8a1bb784c63d3107c5e34be37a81c
Cizzuk <dropcat@cizzuk.net>
2026-08-11 19:30:29 +0900

    Create UserSettings

---
+++ Deflector/Deflector/Models/UserSettings.swift
@@ -0,0 +1,30 @@
+//
+//  UserSettings.swift
+//  Deflector
+//
+//  Created by Cizzuk on 2026/08/11.
+//
+
+import Combine
+import Foundation
+
+final class UserSettings: ObservableObject {
+    static let shared = UserSettings()
+    private init() { }
+
+    private enum Keys {
+        static let sideButtonShortcutName = "sideButtonShortcutName"
+    }
+    
+    @Published var sideButtonShortcutName: String? = {
+        if let value = UserDefaults.standard.string(forKey: Keys.sideButtonShortcutName) {
+            return value
+        }
+        
+        return nil
+    }() {
+        didSet {
+            UserDefaults.standard.set(sideButtonShortcutName, forKey: Keys.sideButtonShortcutName)
+        }
+    }
+}
