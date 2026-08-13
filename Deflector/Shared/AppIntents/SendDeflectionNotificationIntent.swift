3d159a10471109a8e4313e3360c2a68a1d7bdc98
Cizzuk <dropcat@cizzuk.net>
2026-08-13 23:41:45 +0900

    Create SendDeflectionNotificationIntent

---
+++ Deflector/Shared/AppIntents/SendDeflectionNotificationIntent.swift
@@ -0,0 +1,28 @@
+//
+//  SendDeflectionNotificationIntent.swift
+//  Deflector
+//
+//  Created by Cizzuk on 2026/08/12.
+//
+
+import AppIntents
+
+struct SendDeflectionNotificationIntent: AppIntent {
+    static let title: LocalizedStringResource = "Send Deflection Notification"
+    static let isDiscoverable = false
+    static var supportedModes: IntentModes = .background
+    
+    @Parameter(title: "Shortcut Name")
+    var name: String
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        if name.isEmpty {
+            await UserNotificationSupport.sendDeflectionErrorNotification(reason: "Shortcut name is unset")
+        } else {
+            await UserNotificationSupport.sendDeflectionNotification(name: name)
+        }
+        
+        return .result()
+    }
+}
