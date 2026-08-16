05d06d37e59d5d53e6ea2b314107783078bd2815
Cizzuk <dropcat@cizzuk.net>
2026-08-17 00:33:13 +0900

    Update MianView.swift

--- Deflector/Deflector/Views/MianView.swift
+++ Deflector/Deflector/Views/MianView.swift
@@ -23,12 +23,44 @@ struct MianView: View {
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink(destination: SideButtonSettingsView()) {
-                    Label("Side Button", systemImage: "button.vertical.right")
+                Section {
+                    NavigationLink(destination: FirstSetupView()) {
+                        VStack(alignment: .leading) {
+                            Label("First Setup", systemImage: "gearshape")
+                                .font(.title3)
+                                .padding(5)
+                                .padding(.bottom, 5)
+                            Text("To run shortcuts using Deflector, you need to set up an automation in the Shortcuts app.")
+                                .font(.subheadline)
+                            
+                        }
+                    }
                 }
                 
-                NavigationLink(destination: LiveActivitySettingsView()) {
-                    Label("Live Activity", systemImage: "clock.badge")
+                Section {
+                    NavigationLink(destination: LiveActivitySettingsView()) {
+                        VStack(alignment: .leading) {
+                            Label("Live Activity", systemImage: "clock.badge")
+                                .font(.title3)
+                                .padding(5)
+                                .padding(.bottom, 5)
+                            Text("You can set buttons to run shortcuts on the Dynamic Island and the Lock Screen.")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: SideButtonSettingsView()) {
+                        VStack(alignment: .leading) {
+                            Label("Side Button", systemImage: "button.vertical.right")
+                                .font(.title3)
+                                .padding(5)
+                                .padding(.bottom, 5)
+                            Text("Japan-only. You can change the voice assistant assigned to the Side Button. Use a shortcut to access your favorite voice assistant.")
+                                .font(.subheadline)
+                        }
+                    }
                 }
             }
             .navigationTitle("Deflector")
