55b88b4245658c3f345f53f4a17d60248c21f0a8
Cizzuk <dropcat@cizzuk.net>
2026-08-17 00:24:07 +0900

    Update SideButtonSettingsView.swift

--- Deflector/Deflector/Views/ShortcutSettings/SideButtonSettingsView.swift
+++ Deflector/Deflector/Views/ShortcutSettings/SideButtonSettingsView.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct SideButtonSettingsView: View {
     @StateObject private var userSettings = UserSettings.shared
     
+    private var canOpenSettingsURL: Bool {
+        guard URL(string: UIApplication.openSettingsURLString) != nil else { return false }
+        return true
+    }
+    
+    private func openSettingsURL() {
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
+        UIApplication.shared.open(settingsURL)
+    }
+    
     var body: some View {
         List {
             Section {
@@ -17,6 +27,18 @@ struct SideButtonSettingsView: View {
                     .submitLabel(.done)
             } header: {
                 Text("Side Button Shortcut")
+            } footer: {
+                Text("Please enter the name of the shortcut to launch the voice assistant.")
+            }
+            
+            Section {
+                if canOpenSettingsURL {
+                    Button(action: { openSettingsURL() }) {
+                        Label("Open Settings", systemImage: "gear")
+                    }
+                }
+            } footer: {
+                Text("Please set Deflector to the Side Button in Settings.")
             }
         }
         .navigationTitle("Side Button")
