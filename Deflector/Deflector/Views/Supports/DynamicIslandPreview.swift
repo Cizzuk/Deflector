b69c2a6b0c8e3f47e58e74d154201d72aa0d66e9
Cizzuk <dropcat@cizzuk.net>
2026-09-26 22:37:09 +0900

    add makeVerticalIsland

--- Deflector/Deflector/Views/Supports/DynamicIslandPreview.swift
+++ Deflector/Deflector/Views/Supports/DynamicIslandPreview.swift
@@ -15,7 +15,7 @@ struct DynamicIslandPreview: View {
     @Binding var compactLeading: Bool
     @Binding var compactTrailing: Bool
     
-    func makeHorizontalIsland(
+    private func makeHorizontalIsland(
         leading: Bool,
         trailing: Bool,
         centerMargin: CGFloat,
@@ -33,7 +33,7 @@ struct DynamicIslandPreview: View {
                 .stroke(Color(red: 0.16, green: 0.16, blue: 0.16), lineWidth: colorScheme == .dark ? 1.5 : 0)
                 .frame(width: totalWidth, height: 37)
             
-            HStack(spacing: 0) {
+            HStack(alignment: .center, spacing: 0) {
                 if leading {
                     Image(systemName: "suit.diamond")
                         .resizable()
@@ -74,6 +74,53 @@ struct DynamicIslandPreview: View {
         .animation(.bouncy, value: compactTrailing)
     }
     
+    private func makeVerticalIsland(
+        leading: Bool,
+        trailing: Bool,
+        topMargin: CGFloat,
+    ) -> some View {
+        let totalHeight: CGFloat = topMargin + 80
+        return ZStack {
+            Capsule()
+                .fill(Color.black)
+                .stroke(Color(red: 0.16, green: 0.16, blue: 0.16), lineWidth: colorScheme == .dark ? 1.5 : 0)
+                .frame(width: 37, height: totalHeight)
+            
+            VStack(alignment: .center, spacing: 0) {
+                Spacer().frame(height: topMargin)
+                
+                if leading {
+                    Image(systemName: "suit.diamond")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 13)
+                        .padding(.bottom, 11)
+                        .frame(width: 40, height: 40)
+                        .foregroundStyle(.dropblue)
+                        .transition(.blurReplace)
+                } else {
+                    Spacer().frame(width: 40, height: 40)
+                }
+                
+                if trailing {
+                    Image(systemName: leading ? "square.2.layers.3d" : "suit.diamond")
+                        .resizable()
+                        .scaledToFit()
+                        .id(leading ? "square.2.layers.3d" : "suit.diamond")
+                        .padding(.top, 11)
+                        .padding(.bottom, 13)
+                        .frame(width: 40, height: 40)
+                        .foregroundStyle(.dropblue)
+                        .transition(.blurReplace)
+                } else {
+                    Spacer().frame(width: 40, height: 40)
+                }
+            }
+        }
+        .animation(.bouncy, value: compactLeading)
+        .animation(.bouncy, value: compactTrailing)
+    }
+    
     var body: some View {
         switch dynamicIslandType {
         case .horizontal:
@@ -89,7 +136,19 @@ struct DynamicIslandPreview: View {
                 centerMargin: 97
             )
         case .vertical:
-            EmptyView()
+            HStack(alignment: .center, spacing: 0) {
+                makeVerticalIsland(
+                    leading: compactLeading,
+                    trailing: compactTrailing,
+                    topMargin: 40
+                )
+                Spacer().frame(maxWidth: 50)
+                makeVerticalIsland(
+                    leading: compactLeading,
+                    trailing: compactTrailing,
+                    topMargin: 0
+                )
+            }
         default:
             makeHorizontalIsland(
                 leading: compactLeading,
